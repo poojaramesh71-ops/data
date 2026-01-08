50fa9e65a38d1774473796cccc57c24d0b914611
Pooja Ramesh <kumbarp71@gmail.com>
2026-01-08 14:15:47 +0530

    Add files via upload

---
+++ float.py
@@ -0,0 +1,17 @@
+price = float(input("Enter the price of the item: "))
+quantity = int(input("Enter the quantity: "))
+total_cost = price * quantity
+print("Total Cost:", total_cost)
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+mark1 = int(input("Enter marks in subject 1: "))
+mark2 = int(input("Enter marks in subject 2: "))
+mark3 = int(input("Enter marks in subject 3: "))
+
+average_marks = (mark1 + mark2 + mark3) / 3
+
+print("Name:", name)
+print("Age:", age)
+print("Average Marks:", average_marks)
